3192a27561c2e437104b2c462d658a575bba6581
91112021 <dalebolido@gmail.com>
2023-11-16 13:22:27 -0800

    Added more Tests

--- Tests/YTVideoTest.java
+++ Tests/YTVideoTest.java
@@ -37,12 +37,23 @@ class YTVideoTest
     }
 
     @Test
-    void dosomething()
+    void isAnyYoutubeLink() //because there are youtu.be links
     {
         YTVideo yt = new YTVideo();
-        yt.setUrl("https://www.youboot.com/watch?v=U6Z8FkjGEb4&t=2s");
-        assertFalse(yt.getUrl().matches(".*(www\\.youtube\\.com/watch).*"));
+        yt.setUrl("https://youtu.be/zWh3CShX_do?si=PGuK2LO21klWO8wl&t=80");
+        assertTrue(yt.getUrl().matches(".*youtu(\\.be|be\\.com).*"));
     }
+
+
+    @Test
+    void isAnyYoutubeLinkNormal()
+    {
+        YTVideo yt = new YTVideo();
+        yt.setUrl("https://www.youtube.com/watch?v=U6Z8FkjGEb4&t=2s");
+        assertTrue(yt.getUrl().matches(".*youtu(\\.be|be\\.com).*"));
+    }
+
+    @Test
 
 
 
